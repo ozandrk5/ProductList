eefb12acfd68a783136e99018334aa47ec46cb55
Ozan  Doruk <ozandoruk@Ozan-MacBook-Pro.local>
2024-02-23 16:57:37 +0300

    Model ve collectionView oluşturuldu.

---
+++ ProductList/ProductList/ProductCell.swift
@@ -0,0 +1,14 @@
+//
+//  ProductCell.swift
+//  ProductList
+//
+//  Created by Ozan  Doruk on 23.02.2024.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productIV: UIImageView!
+    @IBOutlet weak var productLabel: UILabel!
+}
--- ProductList/ProductList/ViewController.swift
+++ ProductList/ProductList/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var collecitonView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ProductCell
+        cell.productIV.image = UIImage(named: "car")
+        cell.productLabel.text = "Araba"
+        
+        return cell
+    }
+}
